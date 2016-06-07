d54cfe30ac5d44b081ede217b85cb48b4a900671
ylevkovich <ajax.greenweb@gmail.com>
2016-06-07 14:40:47 +0300

    copy in buffer( not finished )

--- views/files/share.php
+++ views/files/share.php
@@ -1,3 +1,30 @@
 <div class="alert alert-success">This code has already copied in your buffer</div>
-<div class="well well-lg"><?=$url;?></div>
+<div class="well well-lg"><a class="js-emaillink" href="1">1</a></div>
 
+
+<!---->
+<!--<p><button class="js-emailcopybtn"><img src="./images/copy-icon.png" /></button></p>-->
+
+<script>
+//    var copyEmailBtn = document.querySelector('.js-emailcopybtn');
+//    copyEmailBtn.addEventListener('click', function(event) {
+        // Выборка ссылки с электронной почтой
+        var emailLink = document.querySelector('.js-emaillink');
+        var range = document.createRange();
+        range.selectNode(emailLink);
+        window.getSelection().addRange(range);
+
+        try {
+            // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copy email command was ' + msg);
+        } catch(err) {
+            console.log('Oops, unable to copy');
+        }
+
+        // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+        // removeRange(range) когда это возможно
+        window.getSelection().removeAllRanges();
+//    });
+</script>
